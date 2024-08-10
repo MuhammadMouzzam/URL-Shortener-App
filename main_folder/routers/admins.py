5bc6a3f91994b7fb1ad2eb7efc8be8d8e07f8ec7
MuhammadMouzzam <mouzzamalimughal2005@gmail.com>
2024-08-10 20:41:21 +0500

    third commit

--- main_folder/routers/admins.py
+++ main_folder/routers/admins.py
@@ -12,7 +12,7 @@ def get_admin_info(admin_key : str, db : Session = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Shortened URL with admin key ({admin_key}) does not exist")
     return db_url
 
-@router.delete('/admin/{admin_key}', status_code=status.HTTP_204_NO_CONTENT)
+@router.delete('/admin/{admin_key}/delete', status_code=status.HTTP_204_NO_CONTENT)
 def delete_URL(admin_key : str, db : Session = Depends(get_db)):
     db_url_query = db.query(models.URL).filter(models.URL.admin_key == admin_key)
     db_url = db_url_query.first()
@@ -20,3 +20,13 @@ def delete_URL(admin_key : str, db : Session = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Shortened URL with admin key ({admin_key}) does not exist")
     db_url_query.delete()
     db.commit()
+
+@router.get('/admin/{admin_key}/deactivate', response_model=schemas.URLInfo)
+def deactivate_URL(admin_key : str, db : Session = Depends(get_db)):
+    db_url = db.query(models.URL).filter(models.URL.admin_key == admin_key).first()
+    if not db_url:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Shortened URL with admin key ({admin_key}) does not exist")
+    db_url.is_active = False
+    db.commit()
+    db.refresh(db_url)
+    return db_url
--- main_folder/routers/forward.py
+++ main_folder/routers/forward.py
@@ -9,15 +9,17 @@ router = APIRouter(tags=['URL Tools'])
 
 @router.get('/{url_key}')
 def forward_to_URL(url_key : str, db : Session = Depends(get_db)):
-    db_url = db.query(models.URL).filter(models.URL.url_key == url_key, models.URL.is_active).first()
+    db_url = db.query(models.URL).filter(models.URL.url_key == url_key).first()
     if not db_url:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Shortened URL ({url_key}) does not exist")
+    if not db_url.is_active:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"{url_key} is not Active")
     if not validators.url(db_url.target_url):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"{db_url.target_url} is not valid")
     utils.visits_update(db_url, db)
     return RedirectResponse(db_url.target_url)
 
-@router.get('/peek/{url_key}', response_model=schemas.URL)
+@router.get('/{url_key}/peek', response_model=schemas.URL)
 def peek_at_URL(url_key : str, db : Session = Depends(get_db)):
     db_url = db.query(models.URL).filter(models.URL.url_key == url_key, models.URL.is_active).first()
     if not db_url:
